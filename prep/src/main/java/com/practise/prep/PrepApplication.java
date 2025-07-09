e22c66db4e09205703d5de92c2d38c0a3d9ff0e2
vicky014 <38026667+vicky014@users.noreply.github.com>
2025-07-09 19:55:34 +0530

    base version

---
+++ prep/src/main/java/com/practise/prep/PrepApplication.java
@@ -0,0 +1,14 @@
+package com.practise.prep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ prep/src/test/java/com/practise/prep/PrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.prep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
