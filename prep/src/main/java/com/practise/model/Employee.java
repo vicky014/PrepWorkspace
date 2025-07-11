0d2f7dabf32be6afa6280d631c873e97ec7ca48f
vicky014 <vickyrams14@gmail.com>
2025-07-11 22:22:33 +0530

    comparator code and streams samples

---
+++ prep/src/main/java/com/practise/model/Employee.java
@@ -0,0 +1,77 @@
+package com.practise.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@AllArgsConstructor
+@Setter
+@Getter
+public class Employee {
+
+	@Id
+	int empId;
+	
+	@Column
+	String name;
+	
+	@Column
+	double percentage;
+	
+	@Column
+	Integer salary;
+
+	public Employee(int empId, String name, double percentage, Integer salary) {
+		super();
+		this.empId = empId;
+		this.name = name;
+		this.percentage = percentage;
+		this.salary = salary;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPercentage() {
+		return percentage;
+	}
+
+	public void setPercentage(double percentage) {
+		this.percentage = percentage;
+	}
+
+	public Integer getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Integer salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", name=" + name + ", percentage=" + percentage + ", salary=" + salary
+				+ "]";
+	}
+	
+	
+}
---
+++ prep/src/main/java/com/practise/workshop/PrepComparator.java
@@ -0,0 +1,118 @@
+package com.practise.workshop;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.practise.model.Employee;
+
+public class PrepComparator {
+
+	public static void main(String[] args) {
+		
+		List<Employee> empList = new ArrayList<>();
+		
+		Employee e1 = new Employee(1, "Vikram", 91.5, 500000);
+		Employee e2 = new Employee(2, "Manju", 81.5, 500000);
+		Employee e3 = new Employee(3, "Vinod", 71.5, 600000);
+		Employee e4 = new Employee(4, "Saikanth", 61.5, 700000);
+
+		empList.add(e1);
+		empList.add(e2);
+		empList.add(e3);
+		empList.add(e4);
+		
+		System.out.println("before formatting");
+		empList.forEach(x -> System.out.println(x));
+//		
+		System.out.println("post sort by name without java 8 ");
+		empList.sort((Employee o1, Employee o2) -> o1.getName().compareTo(o2.getName()));
+		empList.forEach(x -> System.out.println(x));
+		
+//		List<Employee> sortedList = empList.stream()
+//				.sorted(Comparator.comparing(Employee::getName).reversed()
+//				.thenComparing(Employee::getSalary))
+//				.collect(Collectors.toList());
+		
+//		List<Employee> sortedList = empList.stream()
+//				.sorted(Comparator.comparing((Employee e)-> e.getName()).reversed()
+//				.thenComparing((Employee e) -> e.getSalary()))
+//				.collect(Collectors.toList());
+		
+//		List<Employee> sortedList = empList.stream()
+//				.sorted(Comparator.comparing((Employee e)-> e.getName()).reversed())
+//				.collect(Collectors.toList());
+//		System.out.println("post sort by name with java 8");
+//		sortedList.forEach(x -> System.out.println(x));
+//		
+//		sortedList.sort((Employee o1, Employee o2) -> o1.getSalary().compareTo(o2.getSalary()));
+//		System.out.println("post sort by salary with java 8");
+//		sortedList.forEach(x -> System.out.println(x));
+		
+		List<Employee> sortedList = empList.stream()
+				.sorted(Comparator.comparing(Employee::getName).reversed())
+				.sorted(Comparator.comparing(Employee::getSalary))
+				.collect(Collectors.toList());
+		System.out.println("post sort by name with java 8");
+		sortedList.forEach(x -> System.out.println(x));
+		
+		System.out.println("**********");
+		
+//		empList.forEach(x -> System.out.println(x));
+//		List<Employee> sortedList1 = empList.stream().sorted()
+//				.collect(Collectors.toList());
+//		System.out.println("post default sort with java 8");
+//		sortedList1.forEach(x -> System.out.println(x));
+
+//		sortedList.sort((Employee o1, Employee o2) -> o1.getSalary().compareTo(o2.getSalary()));
+//		System.out.println("post sort by salary with java 8");
+//		sortedList.forEach(x -> System.out.println(x));
+		
+		
+		System.out.println("88888888888888888");
+		
+		// Using map
+		List<String> list = Arrays.asList("a,b", "c,d");
+		List<List<String>> mapList = list.stream().map(s -> Arrays.asList(s.split(","))).collect(Collectors.toList());
+		
+		mapList.forEach(x -> System.out.println("Map : "+x));
+		// → List<List<String>>
+
+		// Using flatMap
+		List<String> flatMapList = list.stream().flatMap(s -> Arrays.stream(s.split(","))).collect(Collectors.toList());
+		flatMapList.forEach(x -> System.out.println("Flatmap : "+x));
+		// → List<String>
+
+		List<Integer> intList = Arrays.asList(1,2,3,4,5);
+		intList.stream()
+	    .peek(e -> System.out.println("processing: " + e))
+	    .filter(x -> x > 2)
+	    .count();
+		
+		List<String> resultEmpList = empList.stream()
+        .filter(s -> s.getPercentage() > 60)
+        .sorted(Comparator.comparing(Employee::getName))
+        .limit(3)
+        .map(Employee::getName)
+        .collect(Collectors.toList());
+
+		resultEmpList.forEach(x -> System.out.println(x));
+		
+		List<Employee> resultEmpList1 = empList.stream()
+		        .filter(s -> s.getPercentage() > 60)
+		        .sorted(Comparator.comparing(Employee::getName))
+		        .limit(3)
+//		        .map(Employee::getName)
+		        .collect(Collectors.toList());
+		
+		resultEmpList1.forEach(x -> System.out.println(x));
+
+		
+				
+		
+		
+	}
+
+}
