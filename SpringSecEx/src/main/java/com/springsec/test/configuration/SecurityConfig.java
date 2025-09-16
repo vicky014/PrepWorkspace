b3668a9804a80bb774c4dedcf4d434fb7603a51d
vicky014 <vickyrams14@gmail.com>
2025-09-17 01:46:38 +0530

    Spring Security Security Filter Chain Configuration

--- SpringSecEx/src/main/java/com/springsec/test/configuration/SecurityConfig.java
+++ SpringSecEx/src/main/java/com/springsec/test/configuration/SecurityConfig.java
@@ -2,18 +2,30 @@ package com.springsec.test.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//	@Bean
-//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//		
-//		return http.build();
-//	}
-//}
+@Configuration
+@EnableWebSecurity //this tells Spring that don't use default Spring security but use this
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
+    	http.csrf(csrf -> csrf.disable()); //disabling it for STATLESS REST APi where we dont want to manage session ID's
+    	http.authorizeHttpRequests(req -> req.anyRequest().authenticated());
+//    	http.formLogin(Customizer.withDefaults());
+    	http.httpBasic(Customizer.withDefaults());//to enable rest calls
+    	http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		
+    	/*in browser post this config whenever we login a new request is generated 
+    	and again a login screen is shown to overcome this we have to disable the 
+    	form login and then a alert login popup will be shown and post every refresh 
+    	we get a new session id and we have to manage it*/
+    	return http.build();
+	}
+}
---
+++ SpringSecEx/src/test/java/com/springsec/test/SpringSecExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsec.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
