dd68050eaaceca6ab0ad1ca75c3938acc37bd0d5
vicky014 <vickyrams14@gmail.com>
2025-09-17 00:44:22 +0530

    Kafka Configuration

---
+++ kafka-test/src/main/java/com/kafka_test/service/KafkaMessagePublisher.java
@@ -0,0 +1,14 @@
+package com.kafka_test.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+
+public class KafkaMessagePublisher {
+
+	@Autowired
+	private KafkaTemplate<String, Object> template;
+	
+	public void sendMessageToTopic(String message) {
+		
+	}
+}
